876cd16a85428fd2737fd16c7580074cee7073e3
JoseLazaro <josegabriellazaro27@gmail.com>
2021-11-10 23:08:24 +0800

    Section and Gradle dependencies

--- src/main/java/STSWENGMCO/Section.java
+++ src/main/java/STSWENGMCO/Section.java
@@ -1,4 +1,45 @@
 package STSWENGMCO;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+
+import static org.apache.commons.lang3.Validate.notBlank;
 public class Section {
+
+    private final String sectionId;
+
+    Section (String sectionId) {
+        notBlank(sectionId, "sectionId can't be null or whitespace");
+
+        Validate.isTrue(StringUtils.isAlphanumeric(sectionId),
+                "sectiond must be alphanumeric, was: " + sectionId
+        );
+
+
+		/*if (!StringUtils.isAlphanumeric(sectionId)) {
+			throw new IllegalArgumentException("sectiond must be alphanumeric, was: " + sectionId);
+		}*/
+
+        this.sectionId = sectionId;
+    }
+
+    @Override
+    public String toString() {
+        return sectionId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Section section = (Section) o;
+
+        return sectionId != null ? sectionId.equals(section.sectionId) : section.sectionId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return sectionId != null ? sectionId.hashCode() : 0;
+    }
 }
