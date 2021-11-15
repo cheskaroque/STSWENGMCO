ccf24ddea0a5e103a5e31b58a750993cd665ca5a
JoseLazaro <josegabriellazaro27@gmail.com>
2021-11-15 16:10:24 +0800

    Added comments for requirements needed for iteration 2 in Subject.java

--- src/main/java/STSWENGMCO/Subject.java
+++ src/main/java/STSWENGMCO/Subject.java
@@ -5,6 +5,12 @@ import org.apache.commons.lang3.Validate;
 
 import static org.apache.commons.lang3.Validate.notBlank;
 
+//[ ] A section has a subject. (just 1 subject, not 'block section")
+//[X] A subject is identified by its alphanumeric,Subject ID
+//[ ] A student cannot enlist in two sections with the same subject.
+//[ ] A subject may or may not have one or more prerequisite subjects.
+//[ ] A student may not enlist in a section if the student has not yet taken the prerequisite subjects.
+
 public class Subject {
     private String nameOfSubject;
     private int isOneSubject;
