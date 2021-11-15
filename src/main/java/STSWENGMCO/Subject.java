71a962c9d2748c95cf0e25a04705b9b3562f0f80
cheskaroque <francheska_roque@dlsu.edu.ph>
2021-11-16 02:59:41 +0800

    Removed some comments that are not needed.

--- src/main/java/STSWENGMCO/Subject.java
+++ src/main/java/STSWENGMCO/Subject.java
@@ -3,21 +3,14 @@ package STSWENGMCO;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 
-import java.util.HashSet;
 
 import static org.apache.commons.lang3.Validate.notBlank;
-
-//[X] A section has a subject. (just 1 subject, not 'block section")
-//[X] A subject is identified by its alphanumeric,Subject ID
-//[X] A student cannot enlist in two sections with the same subject.
-//[ ] A subject may or may not have one or more prerequisite subjects.
-//[X] A student may not enlist in a section if the student has not yet taken the prerequisite subjects.
 
 public class Subject {
     private String subjectId;
 
     public Subject(String subjectId){
-        //notBlank(subjectId, "Please provide subject name. Subject name cannot be left blank");
+        notBlank(subjectId, "Please provide subject name. Subject name cannot be left blank");
         Validate.isTrue(StringUtils.isAlphanumeric(subjectId),"Subject Name must be in alphanumeric, was: " + subjectId);
 
         this.subjectId = subjectId;
@@ -40,7 +33,6 @@ public class Subject {
 
     @Override
     public int hashCode() {
-        // return Objects.hash(subjectId);
         return subjectId != null ? subjectId.hashCode() : 0;
     }
 
