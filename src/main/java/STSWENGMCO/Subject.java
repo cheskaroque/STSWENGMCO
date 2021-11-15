0572d8de599102676059911c8cdd2ec350b63881
FrancescaMendoza <francesca_mendoza@dlsu.edu.ph>
2021-11-15 17:00:47 +0800

    Student cannot enlist in the same subject.

--- src/main/java/STSWENGMCO/Subject.java
+++ src/main/java/STSWENGMCO/Subject.java
@@ -6,15 +6,37 @@ import org.apache.commons.lang3.Validate;
 import static org.apache.commons.lang3.Validate.notBlank;
 
 public class Subject {
-    private String nameOfSubject;
+    private String subjectId;
     private int isOneSubject;
+    private boolean getSubj;
 
-    public Subject(String nameOfSubject, int isOneSubject){
+    public Subject(String nameOfSubject, String subjectId){
         notBlank(nameOfSubject, "Please provide subject name. Subject name cannot be left blank");
-        Validate.isTrue(StringUtils.isAlphanumeric(nameOfSubject),"Subject Name must be in alphanumeric, was: " + nameOfSubject);
+        Validate.isTrue(StringUtils.isAlphanumeric(subjectId),"Subject Name must be in alphanumeric, was: " + subjectId);
 
-        this.nameOfSubject = nameOfSubject;
+        this.subjectId = subjectId;
         this.isOneSubject = isOneSubject;
+    }
+
+    @Override
+    public String toString() {
+        return subjectId;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Subject subject = (Subject) o;
+
+        return subjectId != null ? subjectId.equals(subject.subjectId) : subject.subjectId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return subjectId != null ? subjectId.hashCode() : 0;
     }
 
 }
